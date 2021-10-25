0218353a1daec673d0dd1aeeff5cb41d1fe37710
Sina <sina.eft64@gmail.com>
2021-10-25 14:59:24 +0300

    the router beigining

--- src/app.js
+++ src/app.js
@@ -1,7 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import IndecisionApp from './components/IndecisionApp';
+import { BrowserRouter, Route } from "react-router-dom";
 import 'normalize.css/normalize.css'
 import './styles/styles.scss';
+const ExpenseDashboardPage = () =>(
+  <div>
+    This is from my dashboard component.
+  </div>
+);
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'))
+const AddExpensePage = () =>(
+  <div>
+    This is from my Expense component.
+  </div>
+);
+
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={ExpenseDashboardPage} />
+      <Route path="/create" component={AddExpensePage} />
+    </div>
+
+  </BrowserRouter>
+)
+
+ReactDOM.render(routes, document.getElementById('app'))
