da8c4b364bff1f5c8850203e3524a63c5a7cf680
Sina <sina.eft64@gmail.com>
2021-11-14 14:04:42 +0200

    End of sorting the date with react-dates object implemented

--- src/reducers/filters.js
+++ src/reducers/filters.js
@@ -1,9 +1,9 @@
-
+import moment from 'moment';
 const filtersReducerDefaultStatus ={
   text: '',
   sortBy: 'date',
-  startDate: undefined,
-  endDate: undefined
+  startDate: moment().startOf('month'),
+  endDate: moment().endOf('month')
 };
 export default (state = filtersReducerDefaultStatus, action) => {
   switch(action.type){
--- src/selectors/expenses.js
+++ src/selectors/expenses.js
@@ -1,8 +1,10 @@
 // Get visible Expenses:
+import moment from 'moment';
 export default (expenses, {text, sortBy, startDate, endDate}) =>{
   return expenses.filter((expense)=>{
-    const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate;
-    const endDatematch = typeof endDate !== 'number' || expense.createdAt <= endDate;
+    const createdAtMoment = moment(expense.createdAt);
+    const startDateMatch = startDate ? startDate.isSameOrBefore(createdAtMoment, 'day'): true;
+    const endDatematch = endDate ? endDate.isSameOrAfter(createdAtMoment, 'day'): true;
     const textMatch = typeof text !=='string' || expense.description.toLowerCase().includes(text.toLowerCase());
     return startDateMatch && endDatematch && textMatch;
   }).sort((a, b)=>{
