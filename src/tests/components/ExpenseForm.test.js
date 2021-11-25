d893971f45fd515014f294690e786e9e36721f6f
Sina <sina.eft64@gmail.com>
2021-11-25 16:33:13 +0200

    mocked the moment and tested the exoense form with no and one value

---
+++ src/tests/components/ExpenseForm.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import{ shallow } from 'enzyme';
+import ExpenseForm from '../../components/ExpenseForm';
+import expenses from '../fixtures/expenses';
+
+test('should render the expense form correctly',()=>{
+  const wrapper = shallow(<ExpenseForm />);
+  expect(wrapper).toMatchSnapshot();
+});
+test('should render expenseform with expense data',()=>{
+  const wrapper = shallow(<ExpenseForm expense={expenses[0]} />);
+  expect(wrapper).toMatchSnapshot();
+});
