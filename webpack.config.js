745b301d18bacfd0b35138643d9853e1382cc6df
Sina <sina.eft64@gmail.com>
2021-12-12 15:14:28 +0200

    fixed bug with separate db

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,12 @@ const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-if(process.env.NODE_ENV === 'test'){
-  require('dotenv').config({path:'.env.test'});
-}elseif(process.env.NODE_ENV === 'development'){
-  require('dotenv').config({path:'.env.development'});
+if (process.env.NODE_ENV === 'test') {
+  require('dotenv').config({ path: '.env.test' });
+} else if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config({ path: '.env.development' });
 }
+
 module.exports = (env) => {
   const isProduction = env === 'production';
   const CSSExtract = new ExtractTextPlugin('styles.css');
